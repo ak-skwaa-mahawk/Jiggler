3f9be909b22d5c3f77c87bd4d20b56df96a96f73
John Carroll <jcarroll@twomilesolutions.com>
2026-06-03 15:39:53 -0800

    Update SqliteIntentStore.rs

--- src/intent/SqliteIntentStore.rs
+++ src/intent/SqliteIntentStore.rs
@@ -2,21 +2,43 @@ impl SqliteIntentStore {
     pub fn update_band_and_append_event(
         &self,
         band: &IntentBandState,
-        delta: f64,
+        delta_i: f64,
         reason: IntentReason,
     ) -> Result<()> {
         let tx = self.conn.unchecked_transaction()?;
 
-        // update band
+        // Update current intent state
         tx.execute(
-            "...", // same as update_band above
-            params![...],
+            r#"
+            INSERT INTO intent_band (id, mode, intent_value, lastupdatets, source)
+            VALUES (?1, ?2, ?3, ?4, ?5)
+            ON CONFLICT(id, mode) DO UPDATE SET
+                intent_value = excluded.intent_value,
+                lastupdatets = excluded.lastupdatets,
+                source       = excluded.source
+            "#,
+            params![
+                band.band.0,
+                band.mode.0,
+                band.intent_value,
+                band.lastupdatets,
+                band.source
+            ],
         )?;
 
-        // append event
+        // Append to causal log
         tx.execute(
-            "...", // same as append_event above
-            params![...],
+            r#"
+            INSERT INTO intent_event (ts, band, mode, delta_I, reason, meta)
+            VALUES (?1, ?2, ?3, ?4, ?5, '{}')
+            "#,
+            params![
+                band.lastupdatets,
+                band.band.0,
+                band.mode.0,
+                delta_i,
+                reason.as_str()
+            ],
         )?;
 
         tx.commit()?;
