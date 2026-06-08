34089ca2099786dc62bd708229759e7152c9234e
John Carroll <jcarroll@twomilesolutions.com>
2026-06-08 09:55:52 -0800

    Update Manifold_Integration_Logic.rs

--- proto/Manifold_Integration_Logic.rs
+++ proto/Manifold_Integration_Logic.rs
@@ -1,3 +1,35 @@
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct ManifoldTick {
+    #[prost(string, tag="1")]
+    pub vector_id: String,
+
+    #[prost(message, optional, tag="2")]
+    pub economic_vector: Option<EconomicVector>,
+
+    #[prost(message, optional, tag="3")]
+    pub sap_flux_vector: Option<SapFluxVector>,
+
+    #[prost(message, optional, tag="4")]
+    pub magnetic_vector: Option<MagneticVector>,
+
+    #[prost(message, optional, tag="5")]
+    pub biomass_vector: Option<BiomassVector>,
+
+    #[prost(message, optional, tag="6")]
+    pub atmospheric_vector: Option<AtmosphericVector>,
+
+    #[prost(message, optional, tag="7")]
+    pub soil_liquidity_vector: Option<SoilLiquidityVector>,
+
+    #[prost(double, tag="8")]
+    pub vault_depth: f64,
+
+    #[prost(double, tag="9")]
+    pub liquidity_coupling: f64,
+
+    #[prost(int64, tag="10")]
+    pub ts_utc: i64,
+}
 impl ManifoldService {
     pub async fn handle_atmospheric(
         &self,
