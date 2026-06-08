5d96bcf899b4af951e7727bfe55c6800910d559e
John Carroll <jcarroll@twomilesolutions.com>
2026-06-08 09:52:14 -0800

    Update Rust_Struct.rs

--- proto/Rust_Struct.rs
+++ proto/Rust_Struct.rs
@@ -1,4 +1,22 @@
 #[derive(Clone, PartialEq, ::prost::Message)]
+pub struct AtmosphericVector {
+    #[prost(double, tag="1")]
+    pub co2_ppm: f64,
+    #[prost(double, tag="2")]
+    pub o2_percent: f64,
+    #[prost(double, tag="3")]
+    pub humidity_percent: f64,
+    #[prost(double, tag="4")]
+    pub temperature_c: f64,
+    #[prost(double, tag="5")]
+    pub pressure_hpa: f64,
+
+    #[prost(double, tag="6")]
+    pub photosynthetic_efficiency: f64,
+    #[prost(double, tag="7")]
+    pub entropy_load: f64,
+}
+#[derive(Clone, PartialEq, ::prost::Message)]
 pub struct BiomassVector {
     #[prost(double, tag="1")]
     pub canopy_volume_m3: f64,
