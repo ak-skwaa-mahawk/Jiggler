4bbbd1c400552a7ec7e033b715d636c593e9889a
John Carroll <jcarroll@twomilesolutions.com>
2026-06-07 19:46:38 -0800

    Update build.rs

--- build.rs
+++ build.rs
@@ -1,3 +1,12 @@
+cat << 'EOF' > build.rs
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Compiles the new combined proto schema layout into the target OUT_DIR path
+    tonic_compile_io::compile_proto("proto/combined_manifold.proto")?;
+    Ok(())
+}
+EOF
+
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Compile the proto schema into the target directory out-path
     tonic_build::compile_protos("proto/issttoft.proto")?;
