6ee3f0cafab401bcdc64039b9e631bdf70162e05
John Carroll <jcarroll@twomilesolutions.com>
2026-06-08 09:48:59 -0800

    Update ManifoldTick.rs

--- proto/ManifoldTick.rs
+++ proto/ManifoldTick.rs
@@ -12,6 +12,32 @@ pub struct ManifoldTick {
     #[prost(message, optional, tag="4")]
     pub magnetic_vector: Option<MagneticVector>,
 
+    #[prost(message, optional, tag="5")]
+    pub biomass_vector: Option<BiomassVector>,
+
+    #[prost(double, tag="6")]
+    pub vault_depth: f64,
+
+    #[prost(double, tag="7")]
+    pub liquidity_coupling: f64,
+
+    #[prost(int64, tag="8")]
+    pub ts_utc: i64,
+}
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct ManifoldTick {
+    #[prost(string, tag="1")]
+    pub vector_id: String,
+
+    #[prost(message, optional, tag="2")]
+    pub economic_vector: Option<EconomicVector>,
+
+    #[prost(message, optional, tag="3")]
+    pub sap_flux_vector: Option<SapFluxVector>,
+
+    #[prost(message, optional, tag="4")]
+    pub magnetic_vector: Option<MagneticVector>,
+
     #[prost(double, tag="5")]
     pub vault_depth: f64,
 
